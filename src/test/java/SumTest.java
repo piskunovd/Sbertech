674baa6c5a2419e1616bc8021156149efc616d5b
piskunovd <d.piskunov89@gmail.com>
2016-02-26 00:15:19 +0600

    Added files via upload

--- src/test/java/SumTest.java
+++ src/test/java/SumTest.java
@@ -6,23 +6,21 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class SumTest {
     @Test
     public void TestSummation() throws IOException {
-        Str str1 = new Str(0);
-        Str str2 = new Str(1);
-        Str str3 = new Str(2);
-        Str str4 = new Str(3);
-        if (str1.operation.equals("+"))
-            assertEquals(str1.operand1+str1.operand2,str1.result);
-        if (str2.operation.equals("+"))
-            assertEquals(str2.operand1+str2.operand2,str2.result);
-        if (str3.operation.equals("+"))
-            assertEquals(str3.operand1+str3.operand2,str3.result);
-        if (str4.operation.equals("+"))
-            assertEquals(str4.operand1+str4.operand2,str4.result);
+        BufferedReader reader = new BufferedReader(new FileReader("data file.txt"));
+        String line;
+        ArrayList<Str> list = new ArrayList<Str>();
+        for (int i=0;(line=reader.readLine())!=null;i++){
+            String[] parameters = line.split(";");
+            list.add(i,new Str(parameters));
+            if (list.get(i).operation.equals("+"))
+                assertEquals(list.get(i).operand1+list.get(i).operand2,list.get(i).result);
+        }
     }
     public class Str {
         private int operand1;
@@ -30,14 +28,7 @@ public class SumTest {
         private String operation;
         private int result;
 
-        public Str(int j) throws IOException {
-            BufferedReader reader = new BufferedReader(new FileReader("data file.txt"));
-            String line;
-            String [] lines = new String[4];
-            for (int i=0;(line=reader.readLine())!=null;i++)
-                lines[i] = line;
-
-            String[] parameters = lines[j].split(";");
+        public Str(String[] parameters) {
             this.operand1 = Integer.parseInt(parameters[0]);
             this.operand2 = Integer.parseInt(parameters[1]);
             this.operation = parameters[2];
